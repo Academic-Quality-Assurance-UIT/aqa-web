a1a5f79dde2bdc59804910900835d518616c676c
Hoang Hy <hoanghy01122003@gmail.com>
2025-08-06 04:02:29 +0700

    feat: add proxy to backend server

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*", // frontend endpoint
+				destination: "http://localhost:8000/graphql", // backend server
+			},
+		];
+	},
+};
 
 module.exports = nextConfig;
