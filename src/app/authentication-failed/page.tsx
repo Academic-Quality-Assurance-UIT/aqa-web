0a761625af1922ee639fa57d82a10d87da543159
Hoang Hy <hoanghy01122003@gmail.com>
2025-10-10 11:09:37 +0700

    update UI

--- src/app/authentication-failed/page.tsx
+++ src/app/authentication-failed/page.tsx
@@ -1,7 +1,18 @@
-export default function Page() {
+interface PropTypes {
+	searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default function Page({ searchParams }: PropTypes) {
+	const reason = searchParams.reason;
+
 	return (
-		<div>
-			<h1>Không tìm thấy thông tin người dùng</h1>
+		<div className=" w-screen h-screen grid place-items-center">
+			<div className=" flex flex-col items-center gap-2">
+				<h1 className=" text-3xl font-bold">
+					Không tìm thấy thông tin người dùng
+				</h1>
+				{reason && <p>{reason}</p>}
+			</div>
 		</div>
 	);
 }
--- src/components/AuthenticationIntegration.tsx
+++ src/components/AuthenticationIntegration.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useLoginIntegrationMutation } from "@/gql/graphql";
 import { useAuth } from "@/stores/auth.store";
+import { Spinner } from "@heroui/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
@@ -27,17 +28,29 @@ export function AuthenticationIntegration() {
 			console.log({ token });
 
 			const redirectUrl = searchParams.get("redirect");
-			const res = await loginIntegration({ variables: { token }, onError: () => {
-                router.replace('/authentication-failed')
-            } });
-			if (res.data?.loginIntegration) authLogin(res.data?.loginIntegration);
-			if (redirectUrl) router.replace(redirectUrl);
+			const res = await loginIntegration({
+				variables: { token },
+				onError: (data) => {
+					router.push(
+						`authentication-failed?reason=${data.cause?.message}`
+					);
+				},
+			});
+			if (res.data?.loginIntegration) {
+				authLogin(res.data?.loginIntegration);
+				if (redirectUrl) router.replace(redirectUrl);
+			}
 		})();
 	}, [authLogin, loginIntegration, router, searchParams]);
 
 	return (
-		<div>
-			<h1>Đang đăng nhập vào hệ thống...</h1>
+		<div className=" w-screen h-screen grid place-items-center">
+			<div className=" flex items-center gap-2">
+				<Spinner />
+				<h1 className=" text-2xl font-semibold">
+					Đang đăng nhập vào hệ thống...
+				</h1>
+			</div>
 		</div>
 	);
 }
