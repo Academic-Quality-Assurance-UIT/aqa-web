d7ac3a577d1451ac2fb7d1410542d172afa8fa10
Hoang Hy <hoanghy01122003@gmail.com>
2025-10-10 11:16:51 +0700

    update

--- src/app/authentication-failed/page.tsx
+++ src/app/authentication-failed/page.tsx
@@ -9,7 +9,7 @@ export default function Page({ searchParams }: PropTypes) {
 		<div className=" w-screen h-screen grid place-items-center">
 			<div className=" flex flex-col items-center gap-2">
 				<h1 className=" text-3xl font-bold">
-					Không tìm thấy thông tin người dùng
+					Đăng nhập không thành công
 				</h1>
 				{reason && <p>{reason}</p>}
 			</div>
